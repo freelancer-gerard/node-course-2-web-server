0fbf7c7ceb5a211adbbf429c40fda047e050a899
Gerard <freelancergerard@gmail.com>
2018-08-19 00:56:03 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,97 @@
+
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+
+
+var app=express();
+
+
+
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+
+
+app.use((req,res,next)=>{
+	
+	var now=new Date().toString();
+	
+	fs.appendFile('timestamp.txt',now + '\n',(err)=>{
+		
+		if(err)
+			console.log('Error log failed');
+
+	});
+	
+
+	next();
+	
+	
+});
+
+
+app.use((req,res,next)=>{
+
+	res.render('maintenance.hbs');
+
+});
+
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+	
+	return new Date().getFullYear();
+	
+});
+
+hbs.registerHelper('upperCaser',(textMessage)=>{
+	
+	return textMessage.toUpperCase();
+	
+});
+
+
+app.get('/',(req,res)=>{
+	
+	//res.send('<h1>Hello Express</h1>');
+	
+	res.render('home.hbs',{
+		
+		pageTitle:'Home Page',
+		welcomeMessage:'Welcome to our page'
+		
+	});
+	
+	
+});
+
+
+app.get('/about',(req,res)=>{
+	
+	res.render('about.hbs',{
+		
+		pageTitle:'About Page'
+		
+	});
+});
+
+
+app.get('/bad',(req,res)=>{
+	
+	res.send({
+		
+		error:'Unable to Process Page',
+		erro_no:404
+		
+	});
+});
+
+
+
+app.listen(3000,()=>{
+	
+	console.log('Server is Up');
+	
+});
